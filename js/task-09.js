d8ebc2db7348de7d5a818e920453d3a384304050
vladdubrovskyi <vladdubrovskyi1993@gmail.com>
2022-08-06 16:12:49 +0300

    adds 9, starts 10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector(".change-color");
+
+const textEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  textEl.textContent = getRandomHexColor().toString();
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controlContainer = document.querySelector("#controls");
+console.log(controlContainer);
+
+const createBtn = document.querySelector("button[data-create]");
+console.log(createBtn);
+
+const destroyBtn = document.querySelector("button[data-destroy]");
+console.log(destroyBtn);
+
+const inputEl = document.querySelector("div>input");
+console.log(inputEl);
+
+const divBox = document.querySelector("#boxes");
+console.log(divBox);
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    return document.createElement("div");
+  }
+}
+console.log(createBoxes(3));
